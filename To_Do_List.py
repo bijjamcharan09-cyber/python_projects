fd2b8e723bc27e84d3c2129501f0822718349d1b
bijjamcharan09-cyber <bijjamcharan09@gmail.com>
2026-07-18 11:22:19 +0530

    Added the feature of Saving Tasks in a File and added feature of Deleting all tasks(Version: 1.1.0)!

--- To_Do_List.py
+++ To_Do_List.py
@@ -1,52 +1,107 @@
-def display_menu(): #This function displays the main menu options to the user.
+from datetime import datetime
+
+def display_menu():  # This function displays the main menu options to the user.
     print("\n===== TO-DO LIST =====")
     print("1. Add Task")
     print("2. View Tasks")
-    print("3. Exit")
+    print("3. Delete Tasks")
+    print("4. Exit")
 
+def load_tasks(filename="to_do.txt"): #This function loads all the previous tasks in a file.
+    tasks = []
 
-def add_task(tasks): #This function prompts the user to enter a new task and adds it to the list of tasks.
+    try:
+        with open(filename, "r", encoding="utf-8") as file:
+            for line in file:
+                task, date, time = line.strip().split(",")
+
+                tasks.append({
+                    "task": task,
+                    "date": date,
+                    "time": time
+                })
+    except FileNotFoundError:
+        pass
+    except IOError:
+        print("Error loading tasks.")
+
+    return tasks
+
+def add_task(tasks):  # This function prompts the user to enter a new task.
     task = input("Enter a new task: ").strip().capitalize()
 
     if task == "":
         print("Task cannot be empty.")
         return
 
-    tasks.append(task)
+    now = datetime.now()
+
+    tasks.append({
+        "task": task,
+        "date": now.strftime("%d-%m-%Y"),
+        "time": now.strftime("%I:%M:%S %p")
+    })
+
     print("Task added successfully.")
 
+def view_tasks(tasks):  # This function displays all the tasks.
 
-def view_tasks(tasks): #This function displays all the tasks in the list.
     if not tasks:
         print("\nNo tasks available.")
         return
 
     print("\n----- YOUR TASKS -----")
+
     for index, task in enumerate(tasks, start=1):
-        print(f"{index}. {task}")
+        print(
+            f"{index}. Task = {task['task']}\n   "
+            f"Date = {task['date']}\n   "
+            f"Time = {task['time']}"
+        )
 
 
-def main(): #This is the main function that runs the program, displaying the menu and handling user input.
-    tasks = []
+def save_tasks(tasks, filename="to_do.txt"): #This function saves the tasks into a file and uses exception handling.
 
+    try:
+        with open(filename, "w", encoding="utf-8") as file:
+
+            for task in tasks:
+
+                file.write(
+                    f"{task['task']},{task['date']},{task['time']}\n"
+                )
+
+    except IOError:
+        print("Error saving tasks.")
+
+def clear_tasks(tasks, filename="to_do.txt"): # This function clears all the tasks from the list and the file.
+    confirm = input("Are you sure? (yes/no): ").lower()
+    if confirm == "yes":
+        tasks.clear()
+        open(filename, "w").close()
+        print("All tasks cleared!")
+    else:
+        print("Operation cancelled.")
+
+
+def main():  # This is the main function.
+    tasks = load_tasks()
     while True:
         display_menu()
-
         choice = input("Enter your choice: ").strip()
-
-        if choice == "1":
-            add_task(tasks)
-
-        elif choice == "2":
-            view_tasks(tasks)
-
-        elif choice == "3":
-            print("Thank you for using To-Do List.")
-            break
-
-        else:
-            print("Invalid choice. Please try again.")
-
+        match choice:
+             case "1":
+                 add_task(tasks)
+                 save_tasks(tasks,filename="to_do.txt")
+             case "2":
+                 view_tasks(tasks)
+             case "3":
+                 clear_tasks(tasks,filename = "to_do.txt")
+             case "4":
+                 print("Exiting.")
+                 break
+             case "_":
+                 print("Please enter a valid choice.")
 
 if __name__ == "__main__":
     main()

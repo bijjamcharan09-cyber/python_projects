76fcfd41f4d58867bfdf1fffbf15e9b10ad181b0
bijjamcharan09-cyber <bijjamcharan09@gmail.com>
2026-07-22 20:45:53 +0530

    Added the new features copy to clipboard (Version: 1.1.0)!!

--- password_generator.py
+++ password_generator.py
@@ -1,5 +1,6 @@
 import random as rd
 import string as st
+import pyperclip
 
 def get_userappname(): #This function prompts the user to enter a userappname and returns it.
     userappname = input("Enter userappname: ")
@@ -41,6 +42,7 @@ def generate_password(length, include_numbers, include_symbols): #This function 
     rd.shuffle(password)
 
     return "".join(password)
+
 def check_strength(password):
 
     has_upper = False
@@ -70,6 +72,7 @@ def check_strength(password):
 
     else:
         return "Weak"
+
 def save_to_file(userappname, username, password, strength):
 
     file = open("passwords.txt", "a")
@@ -86,17 +89,41 @@ def display_password(userappname, username, password, strength):
     print("Strength :", strength)
 
 def view_saved_passwords():
-    
     try:
         with open("passwords.txt", "r") as file:
-            print("\n----- Saved Passwords -----")
+            print("\n----- Saved Passwords -----\n")
+
+            count = 1
+
             for line in file:
-                print(line.strip())
+                line = line.strip()
+
+                if not line:
+                    continue  # Skip blank lines
+
+                parts = line.split(":")
+
+                if len(parts) < 3:
+                    print(f"Invalid entry: {line}")
+                    continue
+
+                appname = parts[0].strip()
+                username = parts[1].strip()
+                password = parts[2].split(" (")[0].strip()
+                strength = parts[2].split("(")[1].replace(")", "")
+
+                print(f"{count}. App Name : {appname}")
+                print(f"   Username : {username}")
+                print(f"   Password : {password}")
+                print(f"   Strength : {strength}\n")
+
+                count += 1
+
     except FileNotFoundError:
-        print("\nNo saved passwords found.")
+        print("No saved passwords found.")
 
 def delete_saved_password():
-    username = input("Enter username to delete: ").lower()
+    userappname = input("Enter useapprname to delete: ").lower()
 
     found = False
 
@@ -105,9 +132,9 @@ def delete_saved_password():
 
     with open("passwords.txt", "w") as file:
         for line in lines:
-            saved_username = line.split(":")[0].lower()
+            saved_app = line.split(":")[0].lower()
 
-            if saved_username != username:
+            if saved_app != userappname:
                 file.write(line)
             else:
                 found = True
@@ -117,6 +144,77 @@ def delete_saved_password():
     else:
         print("Username not found.")
 
+def copy_to_clipboard(text):
+    try:
+        pyperclip.copy(text)
+        print("-"*20)
+        print("Password copied to clipboard.")
+        print("-"*20)
+    except Exception as e:
+        print("-"*20)
+        print("Error copying to clipboard:",e)
+        print("-"*20)
+
+def ask_and_copy(password):
+    choice = input("Copy password to clipboard? (yes(y)/no(n)): ").lower()
+
+    if choice == "yes" or choice == "y":
+        copy_to_clipboard(password)
+    else:
+        print("-"*20)
+        print("Copy skipped.")
+        print("-"*20)
+
+
+def copy_saved_password():
+    try:
+        with open("passwords.txt", "r") as file:
+            lines = file.readlines()
+
+        if not lines:
+            print("No saved passwords found.")
+            return
+
+        print("\n----- Saved Passwords -----")
+
+        for i, line in enumerate(lines, start=1):
+            line = line.strip()
+
+            if not line:
+                continue
+
+            parts = line.split(":")
+
+            if len(parts) < 3:
+                print(f"Invalid entry: {line}")
+                continue
+
+            appname = parts[0].strip()
+            username = parts[1].strip()
+
+            print(f"{i}. App: {appname} | Username: {username}")
+
+        index = int(input("\nEnter password index: "))
+
+        if 1 <= index <= len(lines):
+            line = lines[index - 1]
+            password = line.split(":")[2].split(" (")[0].strip()
+
+            pyperclip.copy(password)
+
+            print("-" * 20)
+            print("Password copied to clipboard.")
+            print("-" * 20)
+
+        else:
+            print("Invalid index.")
+
+    except FileNotFoundError:
+        print("No saved passwords found.")
+
+    except ValueError:
+        print("Please enter a valid number.")
+
 def main():
 
    while True:
@@ -125,9 +223,11 @@ def main():
         print("1. Generate Password")
         print("2. View Saved Passwords")
         print("3. Delete Saved Password")
-        print("4. Exit")
+        print("4. Copy to clipboard")
+        print("5. Copy saved passwords")
+        print("6. Exit")
 
-        choice = input("Enter your choice (1-4): ")
+        choice = input("Enter your choice (1-6): ")
 
         if choice == "1":
             userappname = get_userappname()
@@ -139,11 +239,20 @@ def main():
                 strength = check_strength(password)
                 display_password(userappname, username, password, strength)
                 save_to_file(userappname, username, password, strength)
+                ask_and_copy(password)
         elif choice == "2":
             view_saved_passwords()
         elif choice == "3":
             delete_saved_password()
+        
         elif choice == "4":
+              if password:
+                 ask_and_copy(password)
+              else:
+                 print("Generate a password first.")
+        elif choice == "5":
+            copy_saved_password()
+        elif choice == "6":
             print("Exiting the program.")
             break
         else:
